4670185729aeca4481b3442fca76ea49a299e34a
ryu2jun <86772235+ryu2jun@users.noreply.github.com>
2024-08-01 13:50:09 +0900

    Update ★백준 11005번.py

--- 파이썬/0801/★백준 11005번.py
+++ 파이썬/0801/★백준 11005번.py
@@ -4,7 +4,7 @@ N, B = map(int, input().split())
 NUM = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 result = ''
-while N: # N이 0이 될때까지
+while N > 0: # N이 0이 될때까지
     result += str(NUM[N%B]) #결과에 N을 36으로 나눈 나머지의 문자열을 더함
     N //= B #N을 B로 나눈 몫으로 초기화
 
