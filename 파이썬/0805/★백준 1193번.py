bae1c4d0ee86fa2a634002651613d0581e06ad98
ryu2jun <86772235+ryu2jun@users.noreply.github.com>
2024-08-06 12:42:14 +0900

    Create ★백준 1193번.py

---
+++ 파이썬/0805/★백준 1193번.py
@@ -0,0 +1,19 @@
+
+X = int(input())
+A = 1
+B = 1
+
+line = 1
+while X > line:
+    X -= line
+    line += 1
+
+if line % 2 == 0:
+    a = X
+    b = line - X + 1
+
+elif line % 2 == 1:
+    a = line - X + 1
+    b = X
+
+print(f'{a}/{b}')
